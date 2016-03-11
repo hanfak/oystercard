c358b4e759453ef58879853e439e4d69f84f4863
eripheebs <erikapheby@gmail.com>
2016-03-11 11:15:05 +0000

    First unit test completed

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,6 +1,12 @@
 require 'oystercard'
 
 describe Oystercard do
-  
+  subject(:oystercard) { described_class.new }
+
+  describe '#initialize' do
+    it 'should initialize with a balance' do
+      expect(oystercard.balance).to eq(0)
+    end
+  end
 
 end
